f20e2f9f0cadf8852b9068c41a39d51dc3f82b1e
Alt0mix <46503091+Alt0mix@users.noreply.github.com>
2019-02-12 17:03:55 -0600

    Update Pneumatics.java

--- src/org/usfirst/frc/team6526/robot/subsystems/Pneumatics.java
+++ src/org/usfirst/frc/team6526/robot/subsystems/Pneumatics.java
@@ -1,26 +1,29 @@
 package org.usfirst.frc.team6526.robot.subsystems;
-//not 100% sure about this code just yet
+//still not 100% sure about this code just yet
 
 import org.usfirst.frc.team6526.robot.RobotMap;
 import edu.wpi.first.wpilibj.Sendable;
 import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
-public class Pneumatics{
+public class Pneumatics extends Subsystem{
 	
-	mainCompressor.setClosedLoopControl(true);
-	mainCompressor.setClosedLoopControl(false);
-	Solenoid piston1 = new Solenoid(1);
-	Solenoid piston2 = new Solenoid(2);
-	mainCompressor.start;
+	public Solenoid piston = new Solenoid(Robot.solenoid);
+	public Compressor compressor = new Compressor();
 	
-	int i = 0;
-	if(i = 0){
-		piston1.set(true);
-		piston2.set(true);
+	public void out(){
+		piston.set(true);
 	}
-	else{
-		piston1.set(false);
-		piston2.set(false);
+	
+	public void in(){
+		piston.set(false);
+	}
+	
+	protected void compStart() {
+		compressor.start;
+	}
+	
+	protected void compStop() {
+		compressor.stop;
 	}
 }
